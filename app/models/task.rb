1da0dbdfd5c79013ce896e0abd9c77d04e67c094
allyssoncomin <allysson_luan@hotmail.com>
2024-12-08 22:20:46 -0300

    Front end

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :user
 
-  validates :title, :description, presence: true
+  validates :title, presence: true, length: { minimum: 3, maximum: 25, too_short: 'deve ter pelo menos %{count} caracteres', too_long: 'deve ter no máximo %{count} caracteres' }
+  validates :description, presence: true, length: { maximum: 255, too_long: 'deve ter no máximo %{count} caracteres' }
   validates :status, inclusion: { in: [true, false] }
 
 end
